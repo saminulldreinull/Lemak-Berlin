6a91262c10bdfcd6328561bdc3ab44da2a325d19
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-02-29 18:28:38 +0100

    HeaderBasement

--- script.js
+++ script.js
@@ -11,30 +11,18 @@ window.onload = () => {
     myFunction(); // Aufruf innerhalb des scroll Event Listeners
   });
 
-  // When the user scrolls the page, execute myFunction
-  function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-  }
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-  document.addEventListener('scroll', function() {
-    var sections = document.querySelectorAll('section');
-    var navLinks = document.querySelectorAll('nav a');
-  
-    sections.forEach((section, index) => {
-      if (window.scrollY >= section.offsetTop - 50) {
-        // Entferne 'active' von allen Links
-        navLinks.forEach((link) => { link.classList.remove('active'); });
-  
-        // Füge 'active' dem entsprechenden Link hinzu
-        var activeLink = document.querySelector(`nav a[href="#${section.id}"]`);
-        if (activeLink) {
-          activeLink.classList.add('active');
-        }
-      }
-    });
+  hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
   });
-  
+
+  document.querySelectorAll(".nav-link").forEach(n => 
+      n.addEventListener("click", () => {
+          hamburger.classList.remove("active");
+          navMenu.classList.remove("active");
+      })
+  );
 };
