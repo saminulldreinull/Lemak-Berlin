fe07b69cdf0560248a1308a45bdbc5f987854209
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-03-01 12:05:52 +0100

    Responsive

--- script.js
+++ script.js
@@ -2,27 +2,9 @@ window.onload = () => {
   window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
     if (scroll > 10) {
-      document.querySelector(".header").classList.add("header-fixed", "shadow");
-      document.querySelector(".logo").classList.add("headers-list");
+      document.querySelector(".header").classList.add( "shadow");
     } else {
-      document.querySelector(".logo").classList.remove("headers-list");
       document.querySelector(".header").classList.remove("header-fixed", "shadow");
-    }
-    myFunction(); // Aufruf innerhalb des scroll Event Listeners
-  });
-
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
-
-  hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-  });
-
-  document.querySelectorAll(".nav-link").forEach(n => 
-      n.addEventListener("click", () => {
-          hamburger.classList.remove("active");
-          navMenu.classList.remove("active");
-      })
-  );
-};
+    }})
+  }
+ 
