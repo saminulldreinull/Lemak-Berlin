6d73078705ac748883e4b1f625eaef22cd3bd299
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-03-07 00:36:32 +0100

    HamburgerMenusOverlflowControl

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    if (scroll > 10) {
-      document.querySelector("header").classList.add("shadow");
-    } else {
-      document.querySelector("header").classList.remove("shadow");
-    }
+  let scroll = this.scrollY;
+  if (scroll > 10) {
+    document.querySelector("header").classList.add("shadow");
+  } else {
+    document.querySelector("header").classList.remove("shadow");
+  }
+});
+
+window.onload = () => {
+  let openMenu = document.querySelector(".open-menu");
+  openMenu.addEventListener("click", (e) => {
+    document.body.style.overflowY = 'hidden';
   });
+  let closeMenu = document.querySelector(".close-menu");
+  closeMenu.addEventListener("click", (e) => {
+    document.body.style.overflowY = 'visible';
+  });
+};
