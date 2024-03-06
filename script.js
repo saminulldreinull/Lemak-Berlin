57c5a411c92c5ab046f00e93aaf686dda8f1994f
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-03-07 00:07:36 +0100

    ResponsiveHamburgerMenu

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     if (scroll > 10) {
-      document.querySelector(".header").classList.add("shadow");
+      document.querySelector("header").classList.add("shadow");
     } else {
-      document.querySelector(".header").classList.remove("shadow");
+      document.querySelector("header").classList.remove("shadow");
     }
   });
