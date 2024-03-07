c9674070a6e3badc31f227ca134a94944c5d09db
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-03-07 01:01:49 +0100

    HamburgerMenuIsWorking

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-window.addEventListener("scroll", (event) => {
-  let scroll = this.scrollY;
-  if (scroll > 10) {
-    document.querySelector("header").classList.add("shadow");
-  } else {
-    document.querySelector("header").classList.remove("shadow");
-  }
-});
-
 window.onload = () => {
   let openMenu = document.querySelector(".open-menu");
+
   openMenu.addEventListener("click", (e) => {
-    document.body.style.overflowY = 'hidden';
+    document.documentElement.style.overflow = 'hidden';
   });
+
   let closeMenu = document.querySelector(".close-menu");
   closeMenu.addEventListener("click", (e) => {
-    document.body.style.overflowY = 'visible';
+    document.documentElement.style.overflow = 'visible';
+  });
+
+  // Füge Event-Listener zu den Navigationslinks hinzu
+  let navLinks = document.querySelectorAll(".nav-item");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      closeMenu.click();  // Ruft den Klick-Event-Handler für den Schließen-Button auf
+    });
   });
 };
